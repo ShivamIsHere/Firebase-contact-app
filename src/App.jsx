d2eee025dc2bdd6c9a001d10460bb41b60517d9a
Shivam <singhkumarshivam15@gmail.com>
2023-12-10 23:37:04 +0530

    fetching data from firebase and displaying in contactcard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from './components/NavBar'
 import {FiSearch} from "react-icons/fi";
 import {AiFillPlusCircle} from "react-icons/ai";
+import {db} from "./config/firebase";
+import {collection, getDocs } from "firebase/firestore"
+import ContactCard from './components/ContactCard';
 
 const App = () => {
+
+
+  const [contacts,setContacts] =useState([]);
+
+  useEffect(()=>{
+    const getContacts = async ()=>{
+
+      try{
+        const contactsRef =collection(db,"contacts")
+        const contactsSnapshot = await getDocs(contactsRef);
+        // console.log(contactsSnapshot)
+        const contactsLists = contactsSnapshot.docs.map((doc)=>{
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        // console.log(contactsLists)
+        setContacts(contactsLists);
+      
+      }catch(error){
+        console.log(error);
+      }
+    };
+    getContacts();
+  },[]);
+
+
+
+
+
   return (
     <div className='max-w-[370px] mx-auto px-4 '>
       <NavBar/>
@@ -16,6 +50,11 @@ const App = () => {
         <AiFillPlusCircle className='text-5xl text-white cursor-pointer'/>
 
       </div>
+      <div className='mt-4'>{
+        contacts.map((contact)=>(
+          <ContactCard key={contact.id} contact={contact}/>
+        ))} 
+        </div>
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <div className='flex items-center justify-center h-[60px] bg-white my-4 rounded-lg gap-2 text-xl font-medium' >
     
-        <img src="/public/logo.png" alt="logo"/>
+        <img src="/logo.png" alt="logo"/>
         <h1>Firebase Contact App</h1>
     
     </div>
