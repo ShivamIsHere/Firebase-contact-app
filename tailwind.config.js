0b4a2887f899b4dc3bd7de0d72c14f5a187376ee
Shivam <singhkumarshivam15@gmail.com>
2023-12-10 21:28:25 +0530

    adding colours to tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ export default {
   ],
   theme: {
     extend: {
-      
+      colors: {
+        gray:"#5A5959",
+        yellow:"#FFEAAE",
+        "dark-yellow":"#FCCA3F",
+        orange:"#F6820C",
+      },
     },
   },
   plugins: [],
