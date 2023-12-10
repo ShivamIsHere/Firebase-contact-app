22381af76f35d2f6e6c81edaa311c918a9f1db1f
Shivam <singhkumarshivam15@gmail.com>
2023-12-10 21:52:09 +0530

    created navbar and fixed the layout size at 370px

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className='max-w-[370px] mx-auto '>
+      <NavBar/>
     </div>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='flex items-center justify-center h-[60px] bg-white m-4 rounded-lg gap-2 text-xl font-medium' >
+    
+        <img src="/public/logo.png" alt="logo"/>
+        <h1>Firebase Contact App</h1>
+    
+    </div>
+  )
+}
+
+export default NavBar
