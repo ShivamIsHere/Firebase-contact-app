92edb09899b75e9f7e2c6ecdcef315e42b8ada5b
Shivam <singhkumarshivam15@gmail.com>
2023-12-17 23:05:15 +0530

    implemented delete function..after deleting we have to referesh page to see the result

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,10 +1,22 @@
+import { doc, deleteDoc} from 'firebase/firestore'
 import React from 'react'
 import { HiOutlineUserCircle } from 'react-icons/hi'
 import { IoMdTrash } from 'react-icons/io'
 import { RiEditCircleLine } from 'react-icons/ri'
+import { db} from "../config/firebase"
 
 const ContactCard = ({contact}) => {
+
+    const deleteContact = async (id)=>{
+      try{
+        await deleteDoc(doc(db,"contacts",id));
+      }catch(error){
+        console.log(error);
+      }
+    };
+  
   return (
+    <>
     <div key={contact.id} className='bg-yellow flex justify-between items-center p-2 rounded-lg'>
           <div className='flex gap-1'>
           <HiOutlineUserCircle className='text-orange text-4xl'/>
@@ -15,9 +27,10 @@ const ContactCard = ({contact}) => {
           </div>
           <div className='flex text-3xl'>
               <RiEditCircleLine/>
-              <IoMdTrash  className='text-orange'/>
+              <IoMdTrash onClick={()=>deleteContact(contact.id)} className='text-orange'/>
           </div>
         </div>
+        </>
   )
 }
 
