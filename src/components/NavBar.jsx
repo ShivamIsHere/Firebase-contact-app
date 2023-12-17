00c9f039a5a9051e1d39c74cbf269da542cf16fc
Shivam <singhkumarshivam15@gmail.com>
2023-12-18 01:31:48 +0530

    deploying

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { IoLogoFirebase } from "react-icons/io5";
 
 const NavBar = () => {
   return (
     <div className='flex items-center justify-center h-[60px] bg-white my-4 rounded-lg gap-2 text-xl font-medium' >
     
-        <img src="/public/logo.png"/>
+        <IoLogoFirebase className='h-12 w-12 text-orange'/>
         <h1>Firebase Contact App</h1>
     
     </div>
--- src/components/NoContactFound.jsx
+++ src/components/NoContactFound.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { IoIosContacts } from "react-icons/io";
 
 const NoContactFound = () => {
   return ( 
     <div className='flex justify-center
     items-center h-[80vh] gap-4'>
         <div>
-        <img src='/public/contact.png'/>
+        <IoIosContacts className="h-20 w-20 text-blue-500"/>
         </div>
       
       <h3 className='text-white text-2xl font-semibold'>Contact Not Found</h3>
